9a2ff09b0bb121fea94bddd7739527ef47186e6f
queralt0cereal07 <queraltros4@gmail.com>
2026-05-11 12:57:11 +0200

    Update3 app.js

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const modeBtn = document.querySelector('.btn.btn-outline-light');
+
 modeBtn.addEventListener('click', () => {
+
     document.body.classList.toggle('dark-mode');
+
     if (document.body.classList.contains('dark-mode')) {
         modeBtn.textContent = 'Light mode';
     } else {
         modeBtn.textContent = 'Dark mode';
     }
+
 });
